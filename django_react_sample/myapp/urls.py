bcf8cb41c37ab27117f456f79fd2ff8fd170037b
fernando <fernando.cillero@a3sec.com>
2017-08-26 17:18:10 +0200

    Remove deprecated way of urls definition

--- django_react_sample/myapp/urls.py
+++ django_react_sample/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
+from myapp.views import post_comment, load_comments, index
 
 urlpatterns = [
-    url(r'^post-comment/$', 'myapp.views.post_comment'),
-    url(r'^load-comments/$', 'myapp.views.load_comments'),
-    url(r'^$', 'myapp.views.index')]
+    url(r'^post-comment/$', post_comment),
+    url(r'^load-comments/$', load_comments),
+    url(r'^$', index)]
